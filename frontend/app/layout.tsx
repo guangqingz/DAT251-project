c5edcffdd7feea708cc2f63cba7ac6b180fa298d
Adele Iren Westrum Kjølstad <aikjolst@stud.ntnu.no>
2026-02-27 12:15:11 +0100

    created navbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Sze Chuan House",
@@ -16,6 +18,14 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
+          <nav className={"flex justify-between items-center mx-6"}>
+              <Image src={"/logo.PNG"} alt={"Logo of the restaurant"} width={100} height={100}/>
+              <ul className={"flex gap-3"}>
+                  <li><Link href="/menu" className={"hover:text-gray-600"}>Menu</Link></li>
+                  <li><Link href="/booking" className={"p-2 bg-gray-400 rounded-md px-3 border-2 border-gray-400 hover:bg-white hover:border-2 hover:border-gray-400"}>Book now</Link></li>
+                  <li><Link href="/contact" className={"p-2 bg-black rounded-md px-3 text-white border-2 hover:bg-white hover:border-2 hover:text-black hover:border-black"}>Contact</Link></li>
+              </ul>
+          </nav>
         {children}
       </body>
     </html>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <section className={"flex flex-col justify-center items-center w-full m-auto mt-10 gap-2"}>
-        <h1 className={"text-red-700 text-6xl font-bold"}>Sze Chuan House</h1>
-        <p className={"text-gray-700 text-xl"}>Nedre Korskirkeallmenningen 9, 5017 Bergen</p>
-        <div className={"flex gap-2"}>
-          <button className={"bg-gray-400 rounded-md px-3"}>Menu</button>
-          <button className={"bg-black text-white rounded-md px-3"}>Book now</button>
-        </div>
-      </section>
-        <div className={"w-full h-96 overflow-hidden flex justify-center"}>
-            <Image src={"/hero.webp"} width={800} height={600} style={{objectFit: "cover"}} alt={"Image of a dish from the restaurant. Deepfried aubergine with special salt."}/>
-        </div>
-    </main>
+    <>
+        <main>
+          <section className={"flex flex-col justify-center items-center w-full m-auto my-10 gap-2"}>
+            <h1 className={"text-red-700 text-6xl font-bold"}>Sze Chuan House</h1>
+            <p className={"text-gray-700 text-xl"}>Nedre Korskirkeallmenningen 9, 5017 Bergen</p>
+            <div className={"flex gap-2"}>
+              <button className={"bg-gray-400 rounded-md px-3"}>Menu</button>
+              <button className={"bg-black text-white rounded-md px-3"}>Book now</button>
+            </div>
+          </section>
+            <div className={"w-full h-96 overflow-hidden flex justify-center"}>
+                <Image src={"/hero.webp"} width={800} height={600} style={{objectFit: "cover"}} alt={"Image of a dish from the restaurant. Deepfried aubergine with special salt."}/>
+            </div>
+        </main>
+      </>
   );
 }
