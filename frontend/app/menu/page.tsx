910889e72b67d011af7ab1f52fad13c90208c2ed
LillyTBui <73482787+LillyTBui@users.noreply.github.com>
2026-03-18 20:51:45 +0100

    feature: create menu section

--- frontend/app/menu/page.tsx
+++ frontend/app/menu/page.tsx
@@ -1,7 +1,12 @@
+import Container from "@/app/ui/Container";
+
 export default function Page () {
     return (
-        <p>
-            This is the menu
-        </p>
+        <section className={"bg-custom-eggwhite min-h-[70dvh] h-full"}>
+            <Container style={"flex flex-col items-center h-full justify-center gap-10"}>
+                <h1 className={"text-3xl lg:text-5xl"}>Vår meny</h1>
+                <a className={"default-btn lg:text-xl font-title bg-custom-red border-custom-red text-white hover:bg-custom-eggwhite hover:text-custom-red"}>Les menyen her</a>
+            </Container>
+        </section>
     )
 }
