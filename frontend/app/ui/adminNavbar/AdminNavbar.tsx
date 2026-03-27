f21727869a845ba0ea619849a14e55923a16fcd9
ErikkaSteen <erioas@hotmail.no>
2026-03-27 21:54:28 +0100

    Added cute emojis on the AdminNavbar

--- frontend/app/ui/adminNavbar/AdminNavbar.tsx
+++ frontend/app/ui/adminNavbar/AdminNavbar.tsx
@@ -1,32 +1,37 @@
 "use client";
 
-import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
-
+import {
+  Squares2X2Icon,
+  CalendarDaysIcon,
+  ArrowRightOnRectangleIcon,
+} from "@heroicons/react/24/outline";
 export default function AdminNavbar() {
   return (
-    <aside className="w-60 bg-[#8B2E1A] rounded-r-2xl text-white flex flex-col  justify-between p-6" >
+    <aside className="w-60 bg-[#8B2E1A] rounded-r-2xl text-white flex flex-col justify-between py-6">
       <div>
         <img
           src="/logo.png"
           alt="Sze Chuan House logo"
-          className="w-[150px] mx-auto mb-5 block"
+          className="w-[170px] mx-auto mb-8 block"
         />
 
-        <nav className="flex flex-col gap-5">
-          <button className="tracking-widest font-title flex items-center gap-2 opacity-85 hover:opacity-100">
-            Dashboard
+        <nav className="flex flex-col">
+          <button className="flex items-center gap-4 px-6 py-4 tracking-widest opacity-100 hover:bg-[#B25533] transition-colors duration-200">
+            <Squares2X2Icon className="w-8 h-8" />
+            <span className="text-l">Dashboard</span>
           </button>
 
-          <button className="tracking-widest flex items-center gap-2 opacity-85 hover:opacity-100 ">
-            Bookings
+          <button className="flex items-center gap-4 px-6 py-4 tracking-widest opacity-100 hover:bg-[#B25533] transition-colors duration-200">
+            <CalendarDaysIcon className="w-8 h-8" />
+            <span className="text-l">Bookings</span>
           </button>
         </nav>
       </div>
 
-    <button className="flex items-center gap-2 opacity-80 hover:opacity-100">
-      <ArrowRightOnRectangleIcon className="w-5 h-5" />
-      <span>Log ut</span>
-    </button>
+      <button className="flex items-center gap-3 px-6 py-3 opacity-100  hover:bg-[#B25533] transition-colors duration-200">
+        <ArrowRightOnRectangleIcon className="w-8 h-8" />
+        <span className="text-l"> Log ut</span>
+      </button>
     </aside>
   );
 }
